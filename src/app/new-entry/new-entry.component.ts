8f4de901344919c4988b36ed797b8d41689878c2
curbyourcode <majlessirouz@gmail.com>
2019-01-04 15:51:36 -0800

    add type array from interface in compoennt

--- src/app/new-entry/new-entry.component.ts
+++ src/app/new-entry/new-entry.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-
+import { Type } from '../interfaces/Type';
 @Component({
   selector: 'app-new-entry',
   templateUrl: './new-entry.component.html',
   styleUrls: ['./new-entry.component.css']
 })
 export class NewEntryComponent {
+  types: Type[] = [
+    {value: true, display: 'Expense'},
+    {value: false, display: 'income'},
+  ];
 
   constructor() { }
 
